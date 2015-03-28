2d76ef845ca402f83774de68742bca7f0668082d
Isaac Wasserman <iwasserblaster@gmail.com>
2015-03-27 23:17:46 -0400

    Port

--- app.js
+++ app.js
@@ -77,11 +77,4 @@ app.get('/m', function (req, res) {
   });
 })
 
-var server = app.listen(3000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
-})
+var server = app.listen(process.env.PORT);
